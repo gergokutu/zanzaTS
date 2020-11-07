ef297aeb3546c443f4b7781a8a01184378603a46
gergokutu <gergo.kutu@gmail.com>
2020-11-07 16:11:06 +0100

    Adjust tslinter.json

--- src/isPalindrome.ts
+++ src/isPalindrome.ts
@@ -1,14 +1,14 @@
 const isPalindrome = (word: string): boolean => {
-  let index: number = 0;
-  let char: string = word[index];
-  const length: number = word.length;
+	let index: number = 0;
+	let char: string = word[index];
+	const length: number = word.length;
 
-  for (let i: number = length - 1; i > 0; i--) {
-    if (word[i] !== char) return false;
-    char = word[++index];
-  }
+	for (let i: number = length - 1; i > 0; i--) {
+		if (word[i] !== char) return false;
+		char = word[++index];
+	}
 
-  return true;
+	return true;
 };
 
 console.log(isPalindrome('faafaaf'));
